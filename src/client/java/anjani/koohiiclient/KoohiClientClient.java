218cb744ea5e7fc7ad3103b96f0cc7861ba52dab
anjanilearns <anjanilearns@gmail.com>
2024-10-09 20:10:09 +0530

    update 4

---
+++ src/client/java/anjani/koohiiclient/KoohiClientClient.java
@@ -0,0 +1,10 @@
+package anjani.koohiiclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KoohiClientClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Your mod's client-side initialization code here
+    }
+}
